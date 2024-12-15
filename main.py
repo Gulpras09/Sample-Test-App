bc3f6745df056848ac5d0e5e063c388c9c6bbeb3
Gulpras09 <prasadyadav.happy1@gmail.com>
2024-12-15 11:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.text("Welcome, Prasad! Your first Streamlit app awaits.")
+
+# Standard imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# 1. Get the data ready
+import pandas as pd
+heart_disease = pd.read_csv(r"C:\Users\91917\Desktop\ML_Projects\01.Backup\DS_ML_Code\ML_Projects\02. Doc\scikit-learn-data (1).csv")
+heart_disease
